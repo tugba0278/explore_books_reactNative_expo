2f368b6af3a9d67f0d1b00ca3ea6a062699b709b
tugba0278 <tugba.oguz.5428@gmail.com>
2024-04-19 16:43:25 +0300

    homepage is created

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,91 @@
-import { useNavigation } from '@react-navigation/core'
-import React, {  useState } from 'react'
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import { auth } from '../firebase'
-import { signInWithEmailAndPassword } from "firebase/auth";
-
+import React, { useEffect, useState } from 'react';
+import { View, Text,  StyleSheet,TouchableOpacity } from 'react-native';
+import { auth, db } from '../firebase';
+import { doc, getDoc } from 'firebase/firestore';
+// Firebase ve diğer gerekli kütüphaneleri ekleyin
 
 const HomeScreen = () => {
-  
-  return (
-    <View>
-        <Text>welcome to homepage</Text>
-    </View>
-      
-  )
-}
-export default HomeScreen
+  const [userName, setUserName] = useState('');
 
+  useEffect(() => {
+    getUserName();
+  }, []);
+
+  const getUserName = async () => {
+    try {
+      const user = auth.currentUser;
+      // Reference to the user's document in Firestore
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      const data = docSnap.data();
+      setUserName(data.Name)
+    } catch (error) {
+      console.error("Hata:", error);
+    }
+  };
+
+  return (
+    <View style={[styles.contanier]}>
+      <Text style={{color:'white',fontSize:30,bottom:30}}>Hoş geldiniz, {userName}!</Text>
+
+      <TouchableOpacity
+        onPress={() => {}}
+        style={ [styles.buttonStyle]}      
+      >
+          <Text style={[styles.textStyle]}>YENİ EKLENENLER</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => {}}
+        style={ [styles.buttonStyle]}      
+      >
+          <Text style={[styles.textStyle]}>ÇOK OKUNANLAR</Text>
+      </TouchableOpacity>
+
+      
+      <TouchableOpacity
+        onPress={() => {}}
+        style={ [styles.buttonStyle]}      
+      >
+          <Text style={[styles.textStyle]}>ÖNERİLENLER</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => {}}
+        style={ [styles.buttonStyle]}      
+      >
+          <Text style={[styles.textStyle]}>FAVORİLER</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+  
+};
+
+export default HomeScreen;
+
+const styles=  StyleSheet.create({
+  contanier:{
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center',
+    backgroundColor:'black'},
+
+  buttonStyle:{
+    backgroundColor: 'white',
+    width: 300,
+    height: 80,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginVertical: 20
+  },
+
+  textStyle:{
+    color: 'black', 
+    fontSize: 20, 
+    fontWeight: 'bold'
+  }
+
+})
